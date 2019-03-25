ac6a77ac91b6606048dbe2760479f76354a9e154
romanhvorostov <romanhvorostov87@gmail.com>
2019-03-25 23:55:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/weartherapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.weartherapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOpenSettings = findViewById(R.id.buttonSettings);
+        btnOpenSettings.setOnClickListener(this);
+
+        Button btnOpenMap = findViewById(R.id.buttonStart);
+        btnOpenMap.setOnClickListener(this);
+    }
+
+    //Один метод для всех кнопок. Внутри метода идет проверка по ID кнопки. Вызываем необходимые активити.
+    public void onClick(View v)
+    {
+        Intent intent;
+        switch (v.getId())
+        {
+            case R.id.buttonStart:
+            {
+                intent = new Intent(this, MapsActivity.class);
+                break;
+            }
+
+            case R.id.buttonSettings:
+            {
+                intent = new Intent(this, Settings.class);
+                break;
+            }
+            default:
+                {
+                    intent = new Intent(this, Settings.class);
+                    break;
+                }
+        }
+        startActivity(intent);
+    }
+}
