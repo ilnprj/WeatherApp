ae5eebf49849708beffb034e114dc5bb6aa8cb1f
romanhvorostov <romanhvorostov87@gmail.com>
2019-03-26 23:47:17 +0700

    Fixes

--- app/src/main/java/com/example/weartherapp/DownloadHandler.java
+++ app/src/main/java/com/example/weartherapp/DownloadHandler.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 import android.content.Context;
 
 public class DownloadHandler {
@@ -23,7 +24,7 @@ public class DownloadHandler {
 
             StringBuffer json = new StringBuffer(1024);
             String tmp = "";
-            while((tmp = reader.readline())!=null)
+            while((tmp = reader.readLine())!=null)
             {
                 json.append(tmp).append("\n");
             }
--- app/src/main/java/com/example/weartherapp/MainActivity.java
+++ app/src/main/java/com/example/weartherapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.weartherapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.Toast;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnOpenSettings = findViewById(R.id.buttonSettings);
         btnOpenSettings.setOnClickListener(this);
+
+        updateWeatherData("Berlin");
     }
 
     //Один метод для всех кнопок. Внутри метода идет проверка по ID кнопки. Вызываем необходимые активити.
@@ -37,5 +43,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
         }
         startActivity(intent);
+    }
+
+    private void updateWeatherData(final String city){
+        new Thread(){
+            public void run(){
+                final JSONObject json = DownloadHandler.getJSON(getApplicationContext(), city);
+                if(json == null){
+                    handler.post(new Runnable(){
+                        public void run(){
+                            Toast.makeText(getApplicationContext(),
+                                    getApplicationContext().getString(R.string.place_not_found),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable(){
+                        public void run(){
+                           // renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
     }
 }
