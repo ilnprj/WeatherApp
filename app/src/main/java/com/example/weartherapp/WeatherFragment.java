948a696c238c6f4220854e7137b3fcc6f938aff3
romanhvorostov <romanhvorostov87@gmail.com>
2019-04-03 22:14:21 +0700

    Добавлены текстовые иконки погоды

--- app/src/main/java/com/example/weartherapp/WeatherFragment.java
+++ app/src/main/java/com/example/weartherapp/WeatherFragment.java
@@ -1,5 +1,6 @@
 package com.example.weartherapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -18,12 +19,13 @@ import java.util.Date;
 import java.util.Locale;
 
 public class WeatherFragment  extends Fragment  {
-
+    Typeface weatherFont;
     TextView cityField;
     TextView temperatureField;
     TextView lastUpdated;
     TextView description ;
     TextView windText;
+    TextView iconWeather;
     Handler handler;
 
     public WeatherFragment()
@@ -39,11 +41,14 @@ public class WeatherFragment  extends Fragment  {
         lastUpdated = rootView.findViewById(R.id.infoText);
         description  = rootView.findViewById(R.id.description );
         windText = rootView.findViewById(R.id.windText);
+        iconWeather = (TextView)rootView.findViewById(R.id.weather_icon);
+        iconWeather.setTypeface(weatherFont);
         return rootView;
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
         updateWeatherData( new UserPrefs(getActivity()).GetDefaultCity());
     }
 
@@ -94,8 +99,47 @@ public class WeatherFragment  extends Fragment  {
             windText.setText(
                     "Wind Speed = "+json.getJSONObject("wind").getString("speed")+"m/s"
             );
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
         }catch(Exception e){
             Log.e("SimpleWeather", "One or more fields not found in the JSON data");
         }
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset)
+    {
+        int id = actualId / 100;
+        String icon = "";
+
+        if (actualId ==  800)
+        {
+            long currentTime = new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset) {
+                icon = getActivity().getString(R.string.weather_sunny);
+            } else {
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        }
+        else
+        {
+            switch (id)
+            {
+                case 2 : icon = getActivity().getString(R.string.weather_thunder);
+                    break;
+                case 3 : icon = getActivity().getString(R.string.weather_drizzle);
+                    break;
+                case 7 : icon = getActivity().getString(R.string.weather_foggy);
+                    break;
+                case 8 : icon = getActivity().getString(R.string.weather_cloudy);
+                    break;
+                case 6 : icon = getActivity().getString(R.string.weather_snowy);
+                    break;
+                case 5 : icon = getActivity().getString(R.string.weather_rainy);
+                    break;
+            }
+        }
+        iconWeather.setText(icon);
     }
 }
