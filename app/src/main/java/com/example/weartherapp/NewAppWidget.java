dbb5ceb1ac16fb3e5fe576a93b0c9cd2a5ca687e
iln <romanhvorostov87@gmail.com>
2019-05-17 22:21:25 +0700

    Вызов окна настроек из виджета.

--- app/src/main/java/com/example/weartherapp/NewAppWidget.java
+++ app/src/main/java/com/example/weartherapp/NewAppWidget.java
@@ -13,6 +13,7 @@ import android.widget.RemoteViews;
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
         // Construct the RemoteViews object
@@ -30,6 +31,14 @@ public class NewAppWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+            Intent configIntent = new Intent(context, Settings.class);
+
+            PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+
+            remoteViews.setOnClickPendingIntent(R.id.widgetViewLogo, configPendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
         }
     }
 
