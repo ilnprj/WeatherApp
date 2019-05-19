0a5446ae2bccf2037d83c2e68ed919112821cd38
iln <romanhvorostov87@gmail.com>
2019-05-19 21:50:07 +0700

    Все необходимые данные загружаются.

--- app/src/main/java/com/example/weartherapp/WidgetLoadData.java
+++ app/src/main/java/com/example/weartherapp/WidgetLoadData.java
@@ -8,14 +8,17 @@ import android.widget.RemoteViews;
 import android.os.Handler;
 import org.json.JSONObject;
 
+import java.util.Date;
+import java.util.Locale;
+
 public class WidgetLoadData {
 
     private AppWidgetManager appWidgetManager;
     private ComponentName watchWidget;
     private RemoteViews remoteViews;
 
-    String lang;
-    Handler handler;
+    private String lang;
+    private Handler handler;
 
     public WidgetLoadData(AppWidgetManager appWidgetManager, ComponentName watchWidget, RemoteViews remoteViews,Context context)
     {
@@ -26,8 +29,8 @@ public class WidgetLoadData {
         updateWeatherData(context);
     }
 
-    public void updateWeatherData(final Context context){
-        final String city = "Samara";
+    private void updateWeatherData(final Context context){
+        final String city = new UserPrefs(context).GetDefaultCity();
         lang = new UserPrefs(context).GetLang();
 
         new Thread(){
@@ -52,12 +55,54 @@ public class WidgetLoadData {
 
     private void renderWeather(JSONObject json){
         try {
+            //Берем из json данные о температуре
             JSONObject main = json.getJSONObject("main");
             remoteViews.setTextViewText(R.id.appwidget_text,String.format("%.2f", main.getDouble("temp"))+ " ℃" );
+
+            //Данные о типе погоды
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            remoteViews.setTextViewText(R.id.typeWeatherWidget,   details.getString("description").toUpperCase());
+
+            //Название города
+            remoteViews.setTextViewText(R.id.cityWeatherWidget,json.getString("name").toUpperCase(Locale.getDefault()));
+
+            //Лого погоды
+            int idWeather = details.getInt("id");
+            long sunrise = json.getJSONObject("sys").getLong("sunrise") * 1000;
+            long sunset = json.getJSONObject("sys").getLong("sunset") * 1000;
+            remoteViews.setImageViewResource(R.id.widgetViewLogo,GetCurrentIcon(idWeather,sunrise,sunset));
+
+            //Обновляем данные в виджете.
             appWidgetManager.updateAppWidget(watchWidget, remoteViews);
-            Log.d("LocConnection  = ","OK");
+
         }catch(Exception e){
-            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+            Log.e("LocConnection", "One or more fields not found in the JSON data");
+        }
+    }
+
+    private int GetCurrentIcon(int actualId,long sunrise,long sunset)
+    {
+        int id = actualId / 100;
+        if (actualId ==  800)
+        {
+            long currentTime = new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset) {
+               return R.drawable.sun;
+            } else {
+                return R.drawable.night;
+            }
+        }
+        else
+        {
+            switch (id)
+            {
+                case 2 : return R.drawable.thunder;
+                case 3 : return R.drawable.rain;
+                case 8 : return R.drawable.cloudy;
+                case 6 : return R.drawable.snow;
+                case 5 : return R.drawable.rain;
+                default: return R.drawable.sun;
+            }
         }
     }
 }
